b9d8867bdffd82d3ccbbbe255e99ddcddfe34508
BenjaminKoshyBiju <bennn455@gmail.com>
2023-11-13 23:50:11 +0530

    edits for  aws

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import route from './mainRoute';
+
 const app = express()
 const port = 4000
 
@@ -7,10 +8,10 @@ import { engine } from 'express-handlebars';
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
- 
 import path from 'path';
 
-app.use(express.static(path.join(__dirname,'static' )))
+
+app.use( express.static('/static'))
 app.use('/', route);
 
 app.listen(port, () => {
--- models/work.ts
+++ models/work.ts
@@ -2,7 +2,7 @@ const work=[
     {
         company:'Software Developer Intern',
         summary: 'Got to work in a Company called Mindlabs as an Intern as part of my final year MCA Course.',
-        date:'Jan 2023 - Feb 2023',
+        date:'Jan 2023 - March 2023',
         detail1:'Worked on a Project called DCMS where I Configured ActiveMQ Artemis and Implemented a SMTP Server in the Project for the seamless transfer of mails during certain tasks in the project.',
         detail2:'Tested APIs using Postman and fixed minor bugs while working on the project.',
         detail3:'Participated in Client Meetings to understand and document Client requirements.',
@@ -11,7 +11,7 @@ const work=[
 
 {
     company:'Digital Marketing Executive',
-    summary: 'Got an Opportunity to work as a Digital Marketing Executive remotely for Wizdots.',
+    summary: 'worked as a Digital Marketing Executive part-time remotely for Wizdots.',
     date:'May 2023 - October 2023',
     detail1:'Created Content ideas for SMM also worked on PPC.',
     detail2:'Created effective Digital Marketing strategies to increase leads and Collaborated with teams working remotely.',
